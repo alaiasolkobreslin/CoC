9a86cd0c4244cad1bc2b143eee00b281ef2b70fa
Alaia Solko-Breslin <ajs644@cornell.edu>
2022-04-30 11:41:16 -0400

    pretty printing

--- src/ast.ml
+++ src/ast.ml
@@ -23,9 +23,20 @@ and stmnt =
 
 type prog = stmnt list
 
-let stmnt_to_sexpr = function 
-| Let (id, t) -> failwith ""
-| Theorem theorem -> failwith ""
-| Expr t -> failwith ""
+let rec t_to_sexpr = function
+| Id id -> SNode id
+| Fun (id, t1, t2) -> SList [SNode "Fun"; SNode id; t_to_sexpr t1; t_to_sexpr t2]
+| App (t1, t2) -> SList [SNode "App"; t_to_sexpr t1; t_to_sexpr t2]
+| Forall (id, t1, t2) -> SList [SNode "Forall"; SNode id; t_to_sexpr t1; t_to_sexpr t2]
+| Type -> SNode "Type"
+
+and theorem_to_sexpr theorem = 
+  SList [SNode "Theorem"; SNode theorem.id; t_to_sexpr theorem.theorem; 
+    SNode "Proof"; t_to_sexpr theorem.proof]
+
+and stmnt_to_sexpr = function 
+| Let (id, t) -> SList [SNode "Let"; SNode id; t_to_sexpr t]
+| Theorem theorem -> theorem_to_sexpr theorem
+| Expr t -> t_to_sexpr t
 
 let prog_to_sexpr prog = SList (List.map stmnt_to_sexpr prog)

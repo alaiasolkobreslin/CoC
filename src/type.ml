b11945bb0c31a3617f41bd8670ea47ea82c7e89b
Alaia Solko-Breslin <ajs644@cornell.edu>
2022-04-30 22:28:28 -0400

    whoops

--- src/type.ml
+++ src/type.ml
@@ -10,9 +10,7 @@ let typecheck_t = function
 | Forall (id, t1, t2) -> failwith "unimplemented"
 | Type -> failwith "unimplemented"
 
-let typecheck_stmnt = function 
+let typecheck_prog = function 
 | Let (id, t, p) -> failwith "unimplemented"
 | Theorem (theorem, p) -> failwith "unimplemented"
 | Expr t -> failwith "unimplemented"
-
-let typecheck prog = failwith "unimplemented"
